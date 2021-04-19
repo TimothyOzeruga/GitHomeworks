e887071b6a00b7a7b3e4e33268f640277a0a2a30
Tim <timaoz900@gmail.com>
2021-04-19 14:15:05 +0300

    11-18 homeworks

---
+++ 17/Cahee/assets/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('#phone').mask('+099 999 99 99');
+});
+
+$(function(){
+    $(window).scroll(function(){
+        console.log($(window).scrollTop());
+        if($(window).scrollTop()>100){
+            $(".header").addClass("fixed");
+        }else{
+            $(".header").removeClass("fixed");
+        }
+    });
+});
+
